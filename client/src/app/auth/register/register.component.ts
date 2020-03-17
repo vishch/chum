7b2e1833e1c8652d8fdd0f0f85bb6afcf60b32c9
Vishal Choudhary <vishal.choudhary@quovantis.com>
2020-03-17 18:45:37 +0530

    Add view password support on password eye click

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2 } from '@angular/core';
 import { AuthOptions } from '../auth-options.model';
 import { AuthMode } from '../auth-mode.enum';
 import { NgForm } from '@angular/forms';
@@ -14,9 +14,23 @@ import { NgForm } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   @Input() authOptions: AuthOptions;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  togglePassword(eyeElem: HTMLElement, passwordElem: HTMLInputElement) {
+    if (eyeElem.classList.contains('fa-eye')) {
+      this.renderer.setAttribute(passwordElem, 'type', 'password');
+      this.renderer.removeClass(eyeElem, 'fa-eye');
+      this.renderer.addClass(eyeElem, 'fa-eye-slash');
+    } else {
+      this.renderer.setAttribute(passwordElem, 'type', 'text');
+      this.renderer.removeClass(eyeElem, 'fa-eye-slash');
+      this.renderer.addClass(eyeElem, 'fa-eye');
+    }
   }
 
   onSubmit(form: NgForm): void {
